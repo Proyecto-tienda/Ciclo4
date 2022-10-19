cdaa12499a9251d12485668afa41f6de157dcb05
Juan Zipaquira <juan444j@gmail.com>
2022-10-19 17:08:54 -0500

    Table and bar menu, boostrap

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,19 @@
-import logo from './logo.svg';
+import {Routes,Route} from 'react-router-dom'
 import './App.css';
+import ListaVentas from './components/ListaVentas';
+import MenuNavegacion from './components/MenuNavegacion'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+    <div className =''>
+        <MenuNavegacion/>
+        <div className='container p-4'>
+            <Routes>
+            <Route path='/' element= {<ListaVentas/>}/>
+            </Routes>
+        </div>
 
+    </div>
+    )
+}
 export default App;
---
+++ frontend/src/components/ListaVentas.js
@@ -0,0 +1,44 @@
+import React from 'react'
+
+const ListaVentas = () => {
+  return (
+<table class="table">
+  <thead class="thead-dark">
+    <tr>
+      <th scope="col">#</th>
+      <th scope="col">Fecha</th>
+      <th scope="col">idVenta</th>
+      <th scope="col">Valor</th>
+    </tr>
+  </thead>
+  <tbody>
+    <tr>
+      <th scope="row">1</th>
+      <td>27/09/2022</td>
+      <td>123</td>
+      <td>$ 30</td>
+    </tr>
+    <tr>
+      <th scope="row">2</th>
+      <td>27/09/2022</td>
+      <td>345</td>
+      <td>$ 40</td>
+    </tr>
+    <tr>
+      <th scope="row">3</th>
+      <td>27/09/2022</td>
+      <td>567</td>
+      <td> $ 60</td>
+    </tr>
+    <tr>
+      <th scope="row">3</th>
+      <td></td>
+      <td>Total</td>
+      <td> $130</td>
+    </tr>
+  </tbody>
+</table>
+  )
+}
+
+export default ListaVentas
