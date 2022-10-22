dbf305f80e63b91821c593018cdcd4c78cf917db
juanquintero97 <juanqg1997@gmail.com>
2022-10-22 12:52:18 -0500

    Corrección en componentes

--- frontend/src/components/ListaVentas.js
+++ frontend/src/components/ListaVentas.js
@@ -1,44 +1,43 @@
-import React from 'react'
+import React, {useState} from 'react'
+import {Table, Button, Row, Col, Container, Image} from "react-bootstrap";
+import ventasJson from "./data/ventas.json";
 
 const ListaVentas = () => {
-  return (
-<table class="table">
-  <thead class="thead-dark">
-    <tr>
-      <th scope="col">#</th>
-      <th scope="col">Fecha</th>
-      <th scope="col">idVenta</th>
-      <th scope="col">Valor</th>
-    </tr>
-  </thead>
-  <tbody>
-    <tr>
-      <th scope="row">1</th>
-      <td>27/09/2022</td>
-      <td>123</td>
-      <td>$ 30</td>
-    </tr>
-    <tr>
-      <th scope="row">2</th>
-      <td>27/09/2022</td>
-      <td>345</td>
-      <td>$ 40</td>
-    </tr>
-    <tr>
-      <th scope="row">3</th>
-      <td>27/09/2022</td>
-      <td>567</td>
-      <td> $ 60</td>
-    </tr>
-    <tr>
-      <th scope="row">3</th>
-      <td></td>
-      <td>Total</td>
-      <td> $130</td>
-    </tr>
-  </tbody>
-</table>
-  )
+    return (
+        <Container>
+            <Row>
+                <Table variant={"hover"} className={"table-bordered"}>
+                    <thead className={"bg-dark text-light text-center"}>
+                    <tr>
+                        <th>Fecha</th>
+                        <th>Id Venta</th>
+                        <th>Valor</th>
+                    </tr>
+                    </thead>
+                    <tbody>
+                    {
+                        ventasJson.map((venta) => {
+                                let {fecha, idCliente, idVenta, valor, confirmado, detalleCompra} = venta;
+                                return (
+                                    <tr>
+                                        <td className={"text-end"}>{fecha}</td>
+                                        <td className={"text-end"}>{idVenta}</td>
+                                        <td className={"text-end"}>$ {valor}</td>
+                                    </tr>
+                                );
+                            }
+                        )
+                    }
+                    <tr>
+                        <td></td>
+                        <td>Total</td>
+                        <td className={"text-end"}>$ Total</td>
+                    </tr>
+                    </tbody>
+                </Table>
+            </Row>
+        </Container>
+    )
 }
 
 export default ListaVentas
