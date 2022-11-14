99299e04962e8521dadf78ca07692fdd2476be3c
juanquintero97 <juanqg1997@gmail.com>
2022-11-13 23:40:01 -0500

    confirmar compra y conocer valor total

--- backend/controllers/cartController.js
+++ backend/controllers/cartController.js
@@ -1,83 +1,89 @@
 const cartSchema = require("../models/cart")
 const product = require("../models/productos")
+
 //View products in cart.
-exports.getCart = async (req,res,next) => {
-    const carts =  await cartSchema.find()
+exports.getCart = async (req, res, next) => {
+    const carts = await cartSchema.find()
     res.status(200).json({
-        sucess:true,
+        sucess: true,
         carts
     })
 }
-/*exports.getCart = async (res,next) => {
-    const cart =  await cartSchema.find()
-    if (!cart){
-        return res.status(200).json({
-        success:true,
-        message: 'No hay productos agregado al carrito',
-    })
-}
-    res.status(200).json({
-        sucess:true,
-        cart
-    })
-}*/
 
 //Add products to cart.
+exports.AddProductToCart = async (req, res) => {
 
-exports.AddProductToCart = async(req,res) => {
-   
-    const { id } = req.body;   
+    const {id} = req.body;
 
     //Does this product exist?
-    const IsProduct = await product.findById( id );
+    const IsProduct = await product.findById(id);
 
 
     //Is Product in the cart?
-    const IsInTheCart = await cartSchema.findOne({ id });
+    const IsInTheCart = await cartSchema.findOne({id});
 
 
     //If product doesn´t exist
-    if(!IsProduct){
+    if (!IsProduct) {
         res.status(404).json({
-            mensaje:'El producto no existe o no esta disponible'
+            mensaje: 'El producto no existe o no esta disponible'
         });
-    }else if(IsInTheCart){
-        ProductCart = await cartSchema.findByIdAndUpdate( id ,{ $inc :{amount : 1 }}, { new:true })
+    } else if (IsInTheCart) {
+        ProductCart = await cartSchema.findByIdAndUpdate(id, {$inc: {amount: 1}}, {new: true})
         res.status(200).json({
             mensaje: "se añadio otro elemento del producto"
         });
-    }else {
-        const {_id, nombre , imagen, precio } = IsProduct
-        const newProductInCart =new cartSchema({name: nombre,imagen : imagen,price:precio ,id_producto:_id, amount : 1});     
-        console.log(newProductInCart)
+    } else {
+        const {_id, nombre, imagen, precio} = IsProduct
+        const newProductInCart = new cartSchema({
+            name: nombre,
+            imagen: imagen,
+            price: precio,
+            id_producto: _id,
+            amount: 1
+        });
         newProductInCart.save()
         res.status(200).json({
-            mensaje : 'Producto agregado al carrito'
+            mensaje: 'Producto agregado al carrito'
         })
+    }
 }
 
 //Delete product to Cart.
-exports.deleteProduct= async (req,res) => {
-    const { name } = req.params.name
+exports.deleteProduct = async (req, res) => {
+    const {name} = req.params.name
     //search product in the cart.
-    const IsProCart =  await cartSchema.findOne({ name })
-    if (!IsProCart){
+    const IsProCart = await cartSchema.findOne({name})
+    if (!IsProCart) {
         res.status(404).json({
-            mensaje : 'El producto no fue encontrado'
+            mensaje: 'El producto no fue encontrado'
         })
-    }
-    else{
-        const DeleteCart = await cartSchema.deleteOne({ name })
-        if(!DeleteCart){
+    } else {
+        const DeleteCart = await cartSchema.deleteOne({name})
+        if (!DeleteCart) {
             res.status(500).json({
-                mesaje : 'Hubo un error, no se pudo eliminar del carrito'
+                mesaje: 'Hubo un error, no se pudo eliminar del carrito'
             })
-        }else{
+        } else {
             res.status(200).json({
-                mesaje : 'Se elimino del carrito el producto'
+                mesaje: 'Se elimino del carrito el producto'
             })
 
-            }
         }
     }
-} 
+}
+
+//Confirmar compra
+exports.confirm = async (req, res) => {
+    const carts = await cartSchema.find()
+    let total = 0;
+    let price = 0;
+    for (cart of carts){
+        price= cart.amount * cart.price
+        total = total +price
+    }
+    res.status(200).json({
+        sucess: true,
+        mensaje: "Desea confirmar su compra por un valor de $" + total + "?"
+    })
+}
--- backend/routes/carts.js
+++ backend/routes/carts.js
@@ -1,10 +1,11 @@
 const express=require("express")
 const router=express.Router()
 
-const { getCart, AddProductToCart, deleteProduct } = require("../controllers/cartController")
+const { getCart, AddProductToCart, deleteProduct, confirm} = require("../controllers/cartController")
 
 router.route('/cart').get(getCart)//Obtener el carrito.
 router.route('/cart/NewProduct').post(AddProductToCart);//Añadir un producto al carrito.
-//router.route('/cart/DeleteProduct/:name').delete(deleteProduct);//Eliminar un producto del carrito atravez del nombre
+router.route('/cart/DeleteProduct/:name').delete(deleteProduct);//Eliminar un producto del carrito atravez del nombre
+router.route('/confirm').get(confirm)
 
 module.exports=router;
