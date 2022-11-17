391e3800069857e98c06f0cbc894c7358694fe34
Juan Zipaquira <juan444j@gmail.com>
2022-11-17 16:24:12 -0500

    Actulizando - corrigiendo errores middleware

--- backend/app.js
+++ backend/app.js
@@ -7,10 +7,11 @@ app.use(express.json());
 const productos=require("./routes/products")
 const cart = require('./routes/carts')
 const ordenes=require("./routes/orders")
+const usuarios = require("./routes/auth")
 
 app.use('/api',productos) //Sujeto a decision (ruta del navegador)
 app.use('/c',cart)
 app.use('/api', ordenes)
-
+app.use('/api',usuarios)
 
 module.exports=app
