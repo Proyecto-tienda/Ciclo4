351363a2f575cfb947036a0adfcfd94e57d0f5e9
Laura Ramirez <layirara@hotmail.com>
2022-11-18 16:53:05 -0500

    Actualizacion front

--- back/models/productos.js
+++ back/models/productos.js
@@ -38,13 +38,13 @@ const productosSchema=mongoose.Schema({
         required:[true,"Por favor seleccione la categoria del producto."],
         enum:{
             values:[
-                "Alimento seco",
-                "Alimento humedo",
-                "Accesorios",
-                "Cuidado e Higiene",
-                "Medicamentos",
-                "Snacks",
-                "Juguetes"
+                "Neveras",
+                "Lavadoras",
+                "Licuadoras",
+                "Batidoras",
+                "Sanducheras",
+                "Televisores",
+                "Equipos de sonido"
             ]
         }
     },
