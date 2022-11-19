0c81f5faafb03c5a2f3c821d88bb32eb72df26eb
Laura Ramirez <layirara@hotmail.com>
2022-11-18 22:43:50 -0500

    Actualizacion front

--- front/src/components/admin/Sidebar.js
+++ front/src/components/admin/Sidebar.js
@@ -6,11 +6,11 @@ const Sidebar = () => {
             <nav className="navbar navbar-expand-lg" id="sidebar">
                 <ul className="navbar-nav">
                     <li className="nav-item">
-                        <Link to="/Dashboard"><i className="fa fa-tachometer"></i> Administración</Link>
+                        <Link to="/Dashboard"><i className="fa fa-cogs"></i> Administración</Link>
                     </li>
                     <li className="nav-item">
                         <a href="#productSubmenu" data-toggle="collapse" aria-expanded="false" className="dropdown"><i
-                            className="fa fa-product-hunt"></i> Productos</a>
+                            className="fa fa-archive"></i> Productos</a>
                         <ul className="collapse list-unstyled" id="productSubmenu">
                             <li>
                                 <Link to="/ProductList"><i className="fa fa-clipboard"></i> Todos</Link>
@@ -23,7 +23,7 @@ const Sidebar = () => {
                     </li >
 
                     <li className="nav-item">
-                        <Link to="/orderList"><i className="fa fa-shopping-basket"></i> Pedidos</Link>
+                        <Link to="/orderList"><i className="fa fa-truck"></i> Pedidos</Link>
                     </li>
 
                     <li className="nav-item">
